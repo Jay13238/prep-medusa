cef18d8ef10eeeec4a8372ac2f28bd78cc0429c2
Jay13238 <115621927+Jay13238@users.noreply.github.com>
2024-11-06 20:45:10 +0200

    "Toggle admin disable condition in medusa-config.ts"

--- medusa-config.ts
+++ medusa-config.ts
@@ -41,7 +41,7 @@ module.exports = defineConfig({
   ],
 
   admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "false",
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
     backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
 });
